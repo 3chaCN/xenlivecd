99174b9820e4f99c79947e1d9a5ea5c18e8f524e
3chaCN <166739292+3chaCN@users.noreply.github.com>
2024-05-28 20:30:20 +0000

    Update 30-remake-squashfs.sh

--- arch-common/30-remake-squashfs.sh
+++ arch-common/30-remake-squashfs.sh
@@ -3,6 +3,6 @@
 apt install -y squashfs-tools
 rm -f binary/live/filesystem.squashfs
 mv binary binary.old
-ln -s /mnt binary
+ln -s /mnt/binary binary
 mv binary.old/* binary/
 mksquashfs chroot binary/live/filesystem.squashfs -e boot initrd.img vmlinuz
