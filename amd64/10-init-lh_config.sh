def7131f206ad536efcabce97b7134baaffcb761
anon <anon@void-live.home>
2024-05-28 12:41:51 +0000

    modified:   10-init-lh_config.sh

--- amd64/10-init-lh_config.sh
+++ amd64/10-init-lh_config.sh
@@ -5,24 +5,27 @@ TODAY=`date +%Y%m%d`
 
 # Run lh_config to prepare all the basic stuff
 lb config \
+    --mode "debian" \
     -a amd64 \
-    -b iso \
     -k "xen-amd64 amd64" \
     -d bookworm \
     --apt apt \
     --apt-recommends true \
     --apt-secure true \
+    --firmware-binary true \
+    --firmware-chroot true \
     --debconf-frontend noninteractive \
     --debconf-priority high \
     --cache true \
-    --bootloaders grub-efi \
+    --bootloaders "syslinux grub-efi" \
     --linux-packages "linux-image aufs-modules squashfs-modules xen-hypervisor xen-linux-system linux-image-xen-amd64 xen-tools" \
+    --binary-image "iso-hybrid" \
     --chroot-filesystem squashfs \
     --mirror-bootstrap http://ftp.fr.debian.org/debian \
     --mirror-chroot http://ftp.fr.debian.org/debian \
     --mirror-chroot-security http://security.debian.org/ \
     --mirror-binary http://ftp.fr.debian.org/debian \
-    --"main contrib non-free" \
+    --archive-areas "main contrib non-free" \
     --checksums true \
     --initramfs live-initramfs \
     --iso-application "Xen Live build $TODAY" \
@@ -30,7 +33,9 @@ lb config \
     --iso-preparer "ben2b <ben2b@proton.me>" \
     --iso-publisher "Xen Live project; http://wiki.xensource.com/xenwiki/LiveDVD; xen-users@lists.xensource.com" \
     --grub-splash "../stuff/splash.xpm.gz" \
-    --source false
+    --source false \
+    --clean \
+    --verbose 
 
 # make the tmp directories
 mkdir tmp ; mkdir -p config/chroot_local-includes/tmp ; chmod 1777 config/chroot_local-includes/tmp
