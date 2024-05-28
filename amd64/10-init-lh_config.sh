f96881c5f48452460e9364af7812ae73b52ffdd9
anon <anon@void-live.home>
2024-05-28 11:52:50 +0000

    modified:   ../README 	modified:   10-init-lh_config.sh 	deleted:    16-get-openvswitch.sh 	modified:   ../arch-common/20-run-lh_build.sh

--- amd64/10-init-lh_config.sh
+++ amd64/10-init-lh_config.sh
@@ -4,46 +4,42 @@
 TODAY=`date +%Y%m%d`
 
 # Run lh_config to prepare all the basic stuff
-lh config \
+lb config \
     -a amd64 \
     -b iso \
     -k "xen-amd64 amd64" \
-    -p standard \
+    -d bookworm \
     --apt apt \
-    --apt-recommends disabled \
-    --apt-secure disable \
+    --apt-recommends true \
+    --apt-secure true \
     --debconf-frontend noninteractive \
     --debconf-priority high \
-    --cache enabled \
-    --bootloader grub \
-    --linux-packages "linux-image-2.6 aufs-modules-2.6 squashfs-modules-2.6 xen-hypervisor xen-linux-system linux-image-xen-amd64 xen-tools" \
-    --packages "squashfs-tools aufs-tools bridge-utils module-assistant build-essential gawk flex libncurses5 debconf-utils debhelper dpkg-dev kernel-package linux-headers-2.6.32-5-xen-amd64 ethtool lvm2 mdadm dmraid google-chrome ntfsprogs ntfs-3g vim screen vlan vtun ssh" \
+    --cache true \
+    --bootloaders grub-efi \
+    --linux-packages "linux-image aufs-modules squashfs-modules xen-hypervisor xen-linux-system linux-image-xen-amd64 xen-tools" \
     --chroot-filesystem squashfs \
-    --union-filesystem aufs \
-    --username livexen \
-    --hostname xenalive \
-    --root-command sudo \
-    --mirror-bootstrap http://ftp.br.debian.org/debian \
-    --mirror-chroot http://ftp.br.debian.org/debian \
+    --mirror-bootstrap http://ftp.fr.debian.org/debian \
+    --mirror-chroot http://ftp.fr.debian.org/debian \
     --mirror-chroot-security http://security.debian.org/ \
-    --mirror-binary http://ftp.br.debian.org/debian \
-    --categories "main contrib non-free" \
-    --checksums enabled \
+    --mirror-binary http://ftp.fr.debian.org/debian \
+    --"main contrib non-free" \
+    --checksums true \
     --initramfs live-initramfs \
-    --distribution lenny \
     --iso-application "Xen Live build $TODAY" \
-    --iso-volume "HPCKP Xen Live DVD v0.1" \
-    --iso-preparer "Jordi Blasco <jordi.blasco@gmail.com>" \
+    --iso-volume "Xen Debian LiveCD v1.0 " \
+    --iso-preparer "ben2b <ben2b@proton.me>" \
     --iso-publisher "Xen Live project; http://wiki.xensource.com/xenwiki/LiveDVD; xen-users@lists.xensource.com" \
     --grub-splash "../stuff/splash.xpm.gz" \
-    --source disabled
+    --source false
 
 # make the tmp directories
 mkdir tmp ; mkdir -p config/chroot_local-includes/tmp ; chmod 1777 config/chroot_local-includes/tmp
  
+cp packages.list config/package-lists/xen.chroot
+
 #    --interactive shell \
 #    --bootstrap-flavour minimal|standard
-#    --cache-indices enabled \
-#    --cache-packages enabled \
-#    --debian-installer enabled|live \
-#    --use-fakeroot enabled \
+#    --cache-indices true \
+#    --cache-packages true \
+#    --debian-installer true|live \
+#    --use-fakeroot true \
--- arch-common/20-run-lh_build.sh
+++ arch-common/20-run-lh_build.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 export MKSQUASHFS_OPTIONS="-e boot initrd.img vmlinuz"
-lh_build
+lb build
