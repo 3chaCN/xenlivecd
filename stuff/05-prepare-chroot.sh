3f04a3e57c08d87bdaa79d42d7f2d51636d7e566
3chaCN <166739292+3chaCN@users.noreply.github.com>
2024-05-29 12:17:47 +0000

    Update 05-prepare-chroot.sh

--- stuff/05-prepare-chroot.sh
+++ stuff/05-prepare-chroot.sh
@@ -6,7 +6,7 @@ sed -i "s/^root::/root:\$1\$XB0iKAYV\$DZM1qDJXQlzDeyszVTipn\/:/" /etc/shadow
 update-alternatives --set editor /usr/bin/vim.basic
 
 ln -s /etc/alternatives/xen-default /usr/lib/xen
-
+mv /etc/grub.d/10_linux /etc/grub.d/50_linux
 rm -f /etc/ssh/ssh_host_*
 
 mkdir -p /var/lib/xen/kernels
