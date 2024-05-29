b95c56d6458469cf0d24019861a4e0e4d74333d1
anon <anon@void-live.home>
2024-05-29 14:58:19 +0000

    modified:   27-update-chroot.sh

--- arch-common/27-update-chroot.sh
+++ arch-common/27-update-chroot.sh
@@ -1,16 +1,16 @@
 #! /bin/sh
 
 # Change the root password to "live":
-sed -i "s/^root::/root:\$1\$XB0iKAYV\$DZM1qDJXQlzDeyszVTipn\/:/" /etc/shadow
+sed -i "s/^root::/root:\$1\$XB0iKAYV\$DZM1qDJXQlzDeyszVTipn\/:/" chroot/etc/shadow
 
-update-alternatives --set editor /usr/bin/vim.basic
+chroot chroot update-alternatives --set editor /usr/bin/vim.basic
 
-ln -s /etc/alternatives/xen-default /usr/lib/xen
-mv /etc/grub.d/10_linux /etc/grub.d/50_linux
-rm -f /etc/ssh/ssh_host_*
+chroot chroot ln -s /etc/alternatives/xen-default /usr/lib/xen
+chroot chroot mv /etc/grub.d/10_linux /etc/grub.d/50_linux
+rm -f chroot/etc/ssh/ssh_host_*
 
-mkdir -p /var/lib/xen/kernels
+mkdir -p chroot/var/lib/xen/kernels
 
-echo "dummy numdummies=6" >> /etc/modules
+echo "dummy numdummies=6" >> chroot/etc/modules
 
-updatedb
+chroot chroot updatedb
