398554cfad8b0ed1a03b0f946fa82f2a85b4c98f
3chaCN <166739292+3chaCN@users.noreply.github.com>
2024-05-29 09:07:47 +0000

    Update 35-remake-iso.sh

--- arch-common/35-remake-iso.sh
+++ arch-common/35-remake-iso.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 echo "nameserver 8.8.8.8" >> /etc/resolv.conf
 echo "nameserver 8.8.8.8" >> chroot/etc/resolv.conf
+apt update
 rm binary/md5sum.txt
 lb binary_checksums --force
 lb binary_iso --force
