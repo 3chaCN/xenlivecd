5eef27a8c7245484a9f3d85c8e936af8f764359e
3chaCN <166739292+3chaCN@users.noreply.github.com>
2024-05-29 06:02:27 +0000

    Update 31-prepare-binary-for-xen.sh

--- amd64/31-prepare-binary-for-xen.sh
+++ amd64/31-prepare-binary-for-xen.sh
@@ -6,10 +6,5 @@
 # can be a good idea to improve this! Then live-helper will be able
 # to generate grub's menu.lst for the live CD from binary/ contents... ;-)
 
-# The package must be downloaded and extracted manually : the xen kernel doesn't
-# exist anymore after image creation
-
 # Copy Hypervisor kernel
-apt download xen-hypervisor-4.17-amd64
-dpkg -x xen-hypervisor-*.deb chroot/
 cp chroot/boot/xen-*.gz binary/live
