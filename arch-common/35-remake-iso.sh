527a8bb4a63d24f20a534eee9f283bd790d4f4f1
3chaCN <166739292+3chaCN@users.noreply.github.com>
2024-05-29 17:08:27 +0000

    Update 35-remake-iso.sh

--- arch-common/35-remake-iso.sh
+++ arch-common/35-remake-iso.sh
@@ -5,7 +5,7 @@ apt update
 
 apt install -y squashfs-tools
 rm -f binary/live/filesystem.squashfs
-mksquashfs chroot binary/live/filesystem.squashfs -e boot initrd.img vmlinuz
+mksquashfs chroot binary/live/filesystem.squashfs -e boot initrd.img-6.1.0-21-amd64 vmlinuz-6.1.0-21-amd64
 
 rm binary/md5sum.txt
 lb binary_checksums --force
