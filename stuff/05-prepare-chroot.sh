58486653e13a6de99a444d319a06f5c04389d944
3chaCN <166739292+3chaCN@users.noreply.github.com>
2024-05-29 08:15:28 +0000

    Update 05-prepare-chroot.sh

--- stuff/05-prepare-chroot.sh
+++ stuff/05-prepare-chroot.sh
@@ -3,9 +3,6 @@
 # Change the root password to "live":
 sed -i "s/^root::/root:\$1\$XB0iKAYV\$DZM1qDJXQlzDeyszVTipn\/:/" /etc/shadow
 
-# Solving bug on Squeeze
-mv /etc/grub.d/10_linux /etc/grub.d/50_linux
-
 update-alternatives --set editor /usr/bin/vim.basic
 
 ln -s /etc/alternatives/xen-default /usr/lib/xen
