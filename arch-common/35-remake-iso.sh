7c344ad200886fef2a0d7d8485d4dcb312b85846
anon <anon@void-live.home>
2024-05-29 14:09:36 +0000

    modified:   35-remake-iso.sh 	deleted:    30-remake-squashfs.sh

--- arch-common/35-remake-iso.sh
+++ arch-common/35-remake-iso.sh
@@ -2,6 +2,11 @@
 echo "nameserver 8.8.8.8" >> /etc/resolv.conf
 echo "nameserver 8.8.8.8" >> chroot/etc/resolv.conf
 apt update
+
+apt install -y squashfs-tools
+rm -f binary/live/filesystem.squashfs
+mksquashfs chroot binary/live/filesystem.squashfs -e boot initrd.img vmlinuz
+
 rm binary/md5sum.txt
 lb binary_checksums --force
 lb binary_iso --force
