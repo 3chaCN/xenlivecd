d6bbe645197ea40d663d7bca52f91fba9a08e65c
3chaCN <166739292+3chaCN@users.noreply.github.com>
2024-06-23 17:28:54 +0200

    Update 27-update-chroot.sh

--- arch-common/27-update-chroot.sh
+++ arch-common/27-update-chroot.sh
@@ -19,7 +19,9 @@ mkdir -p chroot/var/lib/xen/kernels
 cp ../stuff/menu.hook chroot/usr/share/WindowMaker/menu.hook
 cp ../stuff/rules.v4 chroot/etc/iptables/rules.v4
 cp ../stuff/rules.v6 chroot/etc/iptables/rules.v6
-cp ../stuff/*.nmconnection chroot/etc/NetworkManager/system-connections/
+#cp ../stuff/*.nmconnection chroot/etc/NetworkManager/system-connections/
+cp ../stuff/bridge-cfg.sh chroot/etc/NetworkManager/
+cp ../stuff/bridge-cfg.service chroot/lib/systemd/system/
 
 chroot chroot useradd -m G sudo livexen
 cp -vrf ../stuff/Xen/dom0/etc/skel/.Xresources chroot/home/livexen/
